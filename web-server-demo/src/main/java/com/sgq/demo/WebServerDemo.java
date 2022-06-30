d0fdc9a7001a13c539e8228263b39c535c1f012c
Tony <htmlcaptain@gmail.com>
2022-06-30 12:07:47 +0900

    Add hello world  test page: http://localhost:8080/hello

---
+++ web-server-demo/src/main/java/com/sgq/demo/WebServerDemo.java
@@ -0,0 +1,13 @@
+package com.sgq.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServerDemo
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(WebServerDemo.class);
+	}
+}
---
+++ web-server-demo/src/main/java/com/sgq/demo/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.sgq.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public final class TestController
+{
+	@GetMapping("hello")
+	public String hello() {
+		return "Hello World";
+	}
+}
